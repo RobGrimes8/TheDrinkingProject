b53778fe08339c3c29500042d828a01569fcf65d
Rob <grimesrob8@gmail.com>
2020-06-28 13:11:03 +0100

    add edit functionality and validation

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const { render } = require("ejs");
+const { raw } = require("body-parser");
 
 const express = require("express"),
     app = express(),
@@ -24,38 +25,39 @@ const recipeSchema = new mongoose.Schema({
     image: String,
     catagory: [String],
     difficulty: String,
+    rawIngredients: String,
     ingredients: [{ name: String, quantity: String }],
     method: [String],
     deletable: Boolean
 });
 const Recipe = mongoose.model("Recipe", recipeSchema);
 
-mongoose.connect('mongodb+srv://' + process.env.MONGO_LOGIN + '@cluster0-rkzfs.mongodb.net/The_Drinking_Project?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+const drinkCatagories = ["rum", "gin", "vodka", "whiskey", "tequila"];
+mongoose.connect('mongodb+srv://' + process.env.MONGO_LOGIN + '@cluster0-rkzfs.mongodb.net/The_Drinking_Project?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-    // we're connected!
     console.log("Db connected")
-
-
-    // Comment.find(function(err, comments) {
-    //     if (err) return console.error(err);
-    //     console.log(comments);
-    // });
-    // Comment.find({ name: "Beth" }, function(err, comments) {
-    //     if (err) return console.log(err);
-    //     console.log(comments);
-    // });
 });
 
 app.get("/", function(req, res) {
     res.render("landing");
 });
 
+//Index
+app.get("/drinks", function(req, res) {
+    Recipe.find(function(err, recipes) {
+        if (err) return console.log(err);
+        res.render("drinkIndex", { drinks: recipes });
+    })
+});
+
+//New
 app.get("/drinks/new", function(req, res) {
     res.render("newDrink");
 });
 
+//Create
 app.post("/drinks", function(req, res) {
 
 
@@ -69,7 +71,8 @@ app.post("/drinks", function(req, res) {
     let newImage = req.body.newDrinkImg;
     let newCatagories = req.body.newDrinkCatagories;
     let newDifficulty = req.body.newDrinkDifficulty;
-    let newIngredients = stringIntoArrayOfObjects(req.body.newDrinkIngredients);
+    let rawIngredients = req.body.newDrinkIngredients;
+    let newIngredients = stringIntoArrayOfObjects(rawIngredients);
 
     const newRecipe = new Recipe({
         name: newName,
@@ -77,6 +80,7 @@ app.post("/drinks", function(req, res) {
         image: newImage,
         catagory: newCatagories.toLowerCase(),
         difficulty: newDifficulty,
+        rawIngredients: rawIngredients,
         ingredients: newIngredients,
         method: newMethod,
         deletable: true
@@ -90,13 +94,7 @@ app.post("/drinks", function(req, res) {
 
 });
 
-app.get("/drinks", function(req, res) {
-    Recipe.find(function(err, recipes) {
-        if (err) return console.log(err);
-        res.render("drinkIndex", { drinks: recipes });
-    })
-});
-
+//Show (One drink or one catagory)
 app.get("/drinks/:drinkCatagory", function(req, res) {
     if (req.params.drinkCatagory === "surprise") {
         Recipe.find(function(err, drinks) {
@@ -106,26 +104,67 @@ app.get("/drinks/:drinkCatagory", function(req, res) {
             res.render("drink", { drink: randomDrink })
         });
 
-    } else {
+    } else if (drinkCatagories.includes(req.params.drinkCatagory)) {
         Recipe.find({ catagory: req.params.drinkCatagory }, function(err, recipes) {
             if (err) return console.log(err);
             res.render("drinkIndex", { drinks: recipes });
         });
+    } else {
+        Recipe.findById(req.params.drinkCatagory, function(err, recipe) {
+            if (err) return console.log(err);
+            res.render("drink", { drink: recipe })
+        });
     }
 });
 
-app.get("/drinks/:drinkCatagory/:drinkID", function(req, res) {
+//Edit
+app.get("/drinks/:drinkCatagory/:drinkID/edit", function(req, res) {
     Recipe.findById(req.params.drinkID, function(err, recipe) {
         if (err) return console.log(err);
-        res.render("drink", { drink: recipe })
+        res.render("updateDrink", { drink: recipe })
     });
 });
 
+//Update
+app.put("/drinks/:drinkID", function(req, res) {
+    let step1 = req.body.newDrinkMethod1;
+    let step2 = req.body.newDrinkMethod2;
+    let step3 = req.body.newDrinkMethod3;
+
+    let newMethod = [step1, step2, step3];
+    let newName = req.body.newDrinkName;
+    let newDesc = req.body.newDrinkDescription;
+    let newImage = req.body.newDrinkImg;
+    let newCatagories = req.body.newDrinkCatagories;
+    let newDifficulty = req.body.newDrinkDifficulty;
+    let rawIngredients = req.body.newDrinkIngredients;
+    let newIngredients = stringIntoArrayOfObjects(rawIngredients);
+
+    const recipe = {
+        name: newName,
+        desc: newDesc,
+        image: newImage,
+        catagory: newCatagories.toLowerCase(),
+        difficulty: newDifficulty,
+        rawIngredients: rawIngredients,
+        ingredients: newIngredients,
+        method: newMethod,
+        deletable: true
+    }
+
+    Recipe.findByIdAndUpdate(req.params.drinkID, recipe, function(err, updatedRecipe) {
+        res.redirect("/drinks/" + req.params.drinkID);
+    })
+
+});
+
+//Destroy
 app.delete("/drinks/:drinkCatagory/:drinkID", function(req, res) {
     Recipe.findByIdAndDelete(req.params.drinkID, function(err) {
         res.redirect("/");
     });
 });
+
 
 app.listen(process.env.PORT || 3000, function() {
     console.log("server is running");
--- public/scripts/myjQuery.js
+++ public/scripts/myjQuery.js
@@ -6,3 +6,46 @@ $("nav button").click(function() {
         $("nav button i").css("transform", "none");
     }
 });
+
+$(function() {
+
+    $("form[name='newDrinkForm']").validate({
+        // Specify validation rules
+        rules: {
+            newDrinkName: {
+                required: true,
+                maxlength: 20
+            },
+            newDrinkDescription: "required",
+            newDrinkMethod1: "required",
+            newDrinkImg: {
+                required: true,
+                url: true
+            },
+            newDrinkIngredients: "required"
+        },
+        // Specify validation error messages
+        messages: {
+            newDrinkName: {
+                required: "Drink name is required",
+                maxlength: "Name can be no longer than 20 characters sorry!"
+            },
+            newDrinkDescription: "Description is required",
+            newDrinkMethod1: "At least one method is required",
+            newDrinkImg: {
+                required: "Image URL is required",
+                url: "This needs to be a valid URL"
+            },
+            newDrinkCatagories: "A catagory is required",
+            newDrinkIngredients: "An ingredient is required"
+        },
+        errorClass: "invalid",
+        errorElement: "em",
+        // Make sure the form is submitted to the destination defined
+        // in the "action" attribute of the form when valid
+        submitHandler: function(form) {
+            form.submit();
+        }
+    });
+
+});
